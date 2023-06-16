f52a62890e43a24c7db19dfd5f205fba4e86051e
Kristen Swaim <kswaimdev@gmail.com>
2023-06-15 20:26:45 -0400

    extra extra push

--- TimedAsmt.WebAPI/Program.cs
+++ TimedAsmt.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IReplyService, ReplyService>();
+builder.Services.AddScoped<ICommentService,CommentService>();
+builder.Services.AddScoped<IHateService, HateService>();
+builder.Services.AddScoped<IPostService, PostService>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
